6c3b9d8b19e24405cae5aae436bf9f87661cfeea
Johan Beyers <johan@juizi.com>
2024-03-04 22:09:40 +0200

    Add and fix plugins

--- backend/scripts/create_site.py
+++ backend/scripts/create_site.py
@@ -1,65 +1,66 @@
-from AccessControl.SecurityManagement import newSecurityManager
-from p22.interfaces import IP22Layer
-from Products.CMFPlone.factory import _DEFAULT_PROFILE
-from Products.CMFPlone.factory import addPloneSite
-from Testing.makerequest import makerequest
-from zope.interface import directlyProvidedBy
-from zope.interface import directlyProvides
+# from AccessControl.SecurityManagement import newSecurityManager
+# from p22.interfaces import IP22Layer
+# from Products.CMFPlone.factory import _DEFAULT_PROFILE
+# from Products.CMFPlone.factory import addPloneSite
+# from Testing.makerequest import makerequest
+# from zope.interface import directlyProvidedBy
+# from zope.interface import directlyProvides
 
-import os
-import transaction
-
-
-truthy = frozenset(("t", "true", "y", "yes", "on", "1"))
+# import os
+# import transaction
 
 
-def asbool(s):
-    """Return the boolean value ``True`` if the case-lowered value of string
-    input ``s`` is a :term:`truthy string`. If ``s`` is already one of the
-    boolean values ``True`` or ``False``, return it."""
-    if s is None:
-        return False
-    if isinstance(s, bool):
-        return s
-    s = str(s).strip()
-    return s.lower() in truthy
+# truthy = frozenset(("t", "true", "y", "yes", "on", "1"))
 
 
-DELETE_EXISTING = asbool(os.getenv("DELETE_EXISTING"))
+# def asbool(s):
+#     """Return the boolean value ``True`` if the case-lowered value of string
+#     input ``s`` is a :term:`truthy string`. If ``s`` is already one of the
+#     boolean values ``True`` or ``False``, return it."""
+#     if s is None:
+#         return False
+#     if isinstance(s, bool):
+#         return s
+#     s = str(s).strip()
+#     return s.lower() in truthy
 
-app = makerequest(app)  # noQA
 
-request = app.REQUEST
+# DELETE_EXISTING = asbool(os.getenv("DELETE_EXISTING"))
 
-ifaces = [
-    IP22Layer,
-] + list(directlyProvidedBy(request))
+# app = makerequest(app)  # noQA
 
-directlyProvides(request, *ifaces)
+# request = app.REQUEST
 
-admin = app.acl_users.getUserById("admin")
-admin = admin.__of__(app.acl_users)
-newSecurityManager(None, admin)
+# ifaces = [
+#     IP22Layer,
+# ] + list(directlyProvidedBy(request))
 
-site_id = "Plone"
-payload = {
-    "title": "p22",
-    "profile_id": _DEFAULT_PROFILE,
-    "extension_ids": [
-        "p22:default",
-        "p22:initial",
-    ],
-    "setup_content": False,
-    "default_language": "en",
-    "portal_timezone": "America/Sao_Paulo",
-}
+# directlyProvides(request, *ifaces)
 
-if site_id in app.objectIds() and DELETE_EXISTING:
-    app.manage_delObjects([site_id])
-    transaction.commit()
-    app._p_jar.sync()
+# admin = app.acl_users.getUserById("admin")
+# admin = admin.__of__(app.acl_users)
+# newSecurityManager(None, admin)
 
-if site_id not in app.objectIds():
-    site = addPloneSite(app, site_id, **payload)
-    transaction.commit()
-    app._p_jar.sync()
+# site_id = "Plone"
+# payload = {
+#     "title": "p22",
+#     "profile_id": _DEFAULT_PROFILE,
+#     "extension_ids": [
+#         "p22:default",
+#         "p22:initial",
+#     ],
+#     "setup_content": False,
+#     "default_language": "en",
+#     "portal_timezone": "America/Sao_Paulo",
+# }
+
+# if site_id in app.objectIds() and DELETE_EXISTING:
+#     app.manage_delObjects([site_id])
+#     transaction.commit()
+#     app._p_jar.sync()
+
+# if site_id not in app.objectIds():
+#     # site = addPloneSite(app, site_id, **payload)
+#     # transaction.commit()
+#     # app._p_jar.sync()
+#     pass
